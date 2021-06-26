967f80bbaf8beaf8843cd4b0987c302dd89a484e
Oderjunkie <raed.tayyib@gmail.com>
2021-06-27 00:57:20 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/host.js
+++ static/host.js
@@ -1,14 +1,23 @@
+let width, height;
+
 $(()=>{
-    $('.beam').height('50%');
+    width = $('.beam').width();
+    height = $('.beam').height();
+    $('.beam').width('0');
+    $('.beam').height('0');
+    setTimeout(()=>{
+        $('.beam').css({'display': 'initial'});
+        $('.beam').height('50%');
+    }, 1000);
     setTimeout(()=>{
         $('.beam').css({'margin-top': '50vh'});
         $('body').css({'justify-content': 'flex-start'});
         $('.beam').height(0);
-    }, 1000);
+    }, 2000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'center'});
         $('.beam').css({'margin-top': '0px'});
         $('.beam').css({'width': 'initial'});
         $('.beam').css({'height': 'initial'});
-    }, 2000);
+    }, 3000);
 });
