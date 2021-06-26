74c4595c6a89bb463d1879bb6a62b2165345c224
Oderjunkie <raed.tayyib@gmail.com>
2021-06-27 02:33:20 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/host.js
+++ static/host.js
@@ -1,44 +1,46 @@
 function beginAnimation() {
     let width = $('.beam').width();
     let height = $('.beam').height();
-    $('.beam').width('0');
-    $('.beam').height('0');
+    let padding = $('.beam').css('padding');
+    $('.beam').width('0')
+              .height('0');
+              .css({'padding': '0'});
     console.log(1);
     setTimeout(()=>{
-        $('.beam').css({'display': 'initial'});
-        $('.beam').height('50%');
+        $('.beam').css({'display': 'initial'})
+                  .height('50%');
         console.log(2);
     }, 1000);
     setTimeout(()=>{
         $('.beam').css({'margin-top': '50vh'});
+                  .height(0);
         $('body').css({'justify-content': 'flex-start'});
-        $('.beam').height(0);
         console.log(3);
     }, 2000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'center'});
-        $('.beam').css({'margin-top': '0px'});
-        $('.beam').width(width);
-        $('.beam').height(height);
+        $('.beam').css({'margin-top': '0px', 'padding': padding})
+                  .width(width)
+                  .height(height);
         console.log(4);
     }, 3000);
 }
 
 function endAnimation() {
-    $('.beam').width('0');
-    $('.beam').height('0');
+    $('.beam').width('0')
+              .height('0')
+              .css({'padding': '0'});
     console.log(4);
     setTimeout(()=>{
-        $('.beam').css({'margin-top': '50vh'});
+        $('.beam').css({'margin-top': '50vh'})
+                  .height('50%');
         $('body').css({'justify-content': 'flex-start'});
-        $('.beam').height('50%');
         console.log(3);
     }, 1000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'flex-end'});
-        $('.beam').css({'margin-top': '0px'});
-        $('.beam').height(0);
-
+        $('.beam').css({'margin-top': '0px'})
+                  .height(0);
         console.log(2);
     }, 2000);
     setTimeout(()=>{
