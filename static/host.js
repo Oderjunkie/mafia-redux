45cce93a8b2964f2f0dd3a7c9cc0d33389ee2595
Oderjunkie <raed.tayyib@gmail.com>
2021-06-27 01:55:10 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/host.js
+++ static/host.js
@@ -3,38 +3,46 @@ function beginAnimation() {
     let height = $('.beam').height();
     $('.beam').width('0');
     $('.beam').height('0');
+    console.log(1);
     setTimeout(()=>{
         $('.beam').css({'display': 'initial'});
         $('.beam').height('50%');
+        console.log(2);
     }, 1000);
     setTimeout(()=>{
         $('.beam').css({'margin-top': '50vh'});
         $('body').css({'justify-content': 'flex-start'});
         $('.beam').height(0);
+        console.log(3);
     }, 2000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'center'});
         $('.beam').css({'margin-top': '0px'});
         $('.beam').width(width);
         $('.beam').height(height);
+        console.log(4);
     }, 3000);
 }
 
 function endAnimation() {
     $('.beam').width('0');
     $('.beam').height('0');
+    console.log(4);
     setTimeout(()=>{
         $('.beam').css({'margin-top': '50vh'});
         $('body').css({'justify-content': 'flex-start'});
         $('.beam').height('50%');
+        console.log(3);
     }, 1000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'flex-end'});
         $('.beam').css({'margin-top': '0px'});
         $('.beam').height(0);
+        console.log(2);
     }, 2000);
     setTimeout(()=>{
         $('.beam').css({'display': 'none'});
+        console.log(1);
     }, 3000);
 }
 
