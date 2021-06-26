e34a5f60776cc0f84c4bb03561661333525e896a
Oderjunkie <raed.tayyib@gmail.com>
2021-06-26 16:37:58 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/script.js
+++ static/script.js
@@ -1,1 +1,6 @@
-$('#rooms')
+function footer(hasfooter) {
+    if (hasfooter)
+        $('body').removeAttr('footer').attr('nofooter','');
+    else
+        $('body').removeAttr('nofooter').attr('footer','');
+}
