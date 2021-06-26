0806b1da8141128d97a6bfc1abe3f73d00b2f8c5
Oderjunkie <raed.tayyib@gmail.com>
2021-06-27 01:27:36 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/global.js
+++ static/global.js
@@ -5,6 +5,11 @@ function navigateToIndex() {
         newdocument.find('body').find('ul').hide();
         newdocument.find('body').removeAttr('nofooter').attr('footer', '');
                        $('body').removeAttr('footer').attr('nofooter', '');
+        let currentstylesheet = $('link[rel="stylesheet"][href^="/"]');
+        $('head').append($('<link/>').attr({rel: 'stylesheet',
+                                            type: 'text/css',
+                                            href: '/style.css'}));
+        currentstylesheet.remove();
         setTimeout(()=>{
             $('body').append(newdocument.find('body').children());
             $.get('/script.js', (d,s)=>eval(d));
@@ -18,6 +23,11 @@ function navigateToIndex() {
 function navigateToPlay() {
     $.get('/play.html', function(data, status){
         newdocument = $(new DOMParser().parseFromString(data, 'text/html'));
+        let currentstylesheet = $('link[rel="stylesheet"][href^="/"]');
+        $('head').append($('<link/>').attr({rel: 'stylesheet',
+                                            type: 'text/css',
+                                            href: '/play.css'}));
+        currentstylesheet.remove();
         setTimeout(()=>{
             $('body').append(newdocument.find('body').children());
             $.get('/play.js', (d,s)=>eval(d));
@@ -28,6 +38,11 @@ function navigateToPlay() {
 function navigateToHost() {
     $.get('/host.html', function(data, status){
         newdocument = $(new DOMParser().parseFromString(data, 'text/html'));
+        let currentstylesheet = $('link[rel="stylesheet"][href^="/"]');
+        $('head').append($('<link/>').attr({rel: 'stylesheet',
+                                            type: 'text/css',
+                                            href: '/host.css'}));
+        currentstylesheet.remove();
         setTimeout(()=>{
             $('body').append(newdocument.find('body').children());
             $.get('/host.js', (d,s)=>eval(d));
