6b3c82ced1d1930b3fc36d6c3ccbe6ecdb1009e0
Oderjunkie <raed.tayyib@gmail.com>
2021-06-27 22:54:07 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/specific.js
+++ static/specific.js
@@ -1,24 +1,27 @@
 let roomid, socket, button, textbox, form;
 
+function checkforsubmission(e) {
+    let disabled = button.attr('disabled');
+    let text = textbox.val();
+    if (text && disabled)
+        button.removeAttr('disabled');
+    else if (!text && !disabled)
+        button.attr('disabled', '');
+}
+
 $(_=>{
     roomid = location.pathname.split('/')[2];
     form = $('.input');
     button = $('.input > input[type=submit]');
     textbox = $('.input > input[type=text]');
-    textbox.on('input', e=>{
-        let disabled = button.attr('disabled');
-        let text = textbox.val();
-        if (text && disabled)
-            button.removeAttr('disabled');
-        else if (!text && !disabled)
-            button.attr('disabled', '');
-    }).keypress(e=>{
+    textbox.on('input', checkforsubmission).keypress(e=>{
         if (e.which==13)
             button.click();
     });
     form.submit(()=>{
         socket.emit('chat', textbox.val());
         textbox.val('');
+        checkforsubmission();
         return false;
     });
     socket = io();
