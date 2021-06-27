5d3d51bd6661c9e6b78883473af35118f45f7072
Oderjunkie <raed.tayyib@gmail.com>
2021-06-27 04:16:35 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- static/host.js
+++ static/host.js
@@ -1,50 +1,50 @@
 function beginAnimation() {
-    let width = $('.beam').width();
-    let height = $('.beam').height();
-    let padding = $('.beam').css('padding');
-    $('.beam').width('0')
-              .height('0')
-              .css({'padding': '0'});
+    let width = $('form').width();
+    let height = $('form').height();
+    let padding = $('form').css('padding');
+    $('form').width('0')
+             .height('0')
+             .css({'padding': '0'});
     console.log(1);
     setTimeout(()=>{
-        $('.beam').css({'display': 'initial'})
-                  .height('50%');
+        $('form').css({'display': 'initial'})
+                 .height('50%');
         console.log(2);
     }, 1000);
     setTimeout(()=>{
-        $('.beam').css({'margin-top': '50vh'})
-                  .height(0);
+        $('form').css({'margin-top': '50vh'})
+                 .height(0);
         $('body').css({'justify-content': 'flex-start'});
         console.log(3);
     }, 2000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'center'});
-        $('.beam').css({'margin-top': '0px', 'padding': padding})
-                  .width(width)
-                  .height(height);
+        $('form').css({'margin-top': '0px', 'padding': padding})
+                 .width(width)
+                 .height(height);
         console.log(4);
     }, 3000);
 }
 
 function endAnimation() {
-    $('.beam').width('0')
-              .height('0')
-              .css({'padding': '0'});
+    $('form').width('0')
+             .height('0')
+             .css({'padding': '0'});
     console.log(4);
     setTimeout(()=>{
-        $('.beam').css({'margin-top': '50vh'})
-                  .height('50%');
+        $('form').css({'margin-top': '50vh'})
+                 .height('50%');
         $('body').css({'justify-content': 'flex-start'});
         console.log(3);
     }, 1000);
     setTimeout(()=>{
         $('body').css({'justify-content': 'flex-end'});
-        $('.beam').css({'margin-top': '0px'})
-                  .height(0);
+        $('form').css({'margin-top': '0px'})
+                 .height(0);
         console.log(2);
     }, 2000);
     setTimeout(()=>{
-        $('.beam').css({'display': 'none'});
+        $('form').css({'display': 'none'});
         console.log(1);
     }, 3000);
 }
