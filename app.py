087ce94d7b9d4b71d2b16aff8f187d7f9b0c3fb8
Oderjunkie <raed.tayyib@gmail.com>
2021-07-02 04:43:11 +0300

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- app.py
+++ app.py
@@ -54,11 +54,11 @@ client = pymongo.MongoClient(os.environ['MONGODB'])
 # Index page + scripts
 #######################
 
-#@app.route('/')
+@app.route('/')
 #def index():
 #    return app.send_static_file('under_renovation.html')
 
-@app.route('/')
+@app.route('/index.html')
 def index2():
     return app.send_static_file('index.html')
 
