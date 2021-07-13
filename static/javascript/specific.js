01484f22276660bbb7c737ffc4861032188923e9
Oderjunkie <raed.tayyib@gmail.com>
2021-07-13 14:57:33 +0300

    Adding the front-end components for playing and hosting games

--- static/javascript/specific.js
+++ static/javascript/specific.js
@@ -1,22 +1,21 @@
-let roomid, socket, button, textbox, form, chat;
+let roomid, socket, button, textbox, form, chat, packets;
 
-const checkforsubmission = e=>{
-    let disabled = button.attr('disabled');
+function checkforsubmission() {
     let text = textbox.val();
-    if (text && disabled)
-        button.removeAttr('disabled');
-    else if (!text && !disabled)
-        button.attr('disabled', '');
-};
+    if (text)
+        button.prop('disabled', false);
+    else if (!text)
+        button.prop('disabled', true);
+}
 
-const convertTimeToElement = msg=>{
+function convertTimeToElement() {
     let date = new Date(msg.timestamp*1000);
     $('<time></time>').append(
         `${date.getHours().toString().padStart(2, '0')}:`+
         `${date.getMinutes().toString().padStart(2, '0')}:`+
         `${date.getSeconds().toString().padStart(2, '0')}`
     );
-};
+}
 
 function addToChat(el) {
     let chatobj = chat[0];
@@ -27,6 +26,7 @@ function addToChat(el) {
         chat.scrollTop(newmax);
     }
 }
+
 function createChatFrom(msg) {
     return $('<message></message>').append(
         convertTimeToElement(msg),
@@ -34,20 +34,33 @@ function createChatFrom(msg) {
         msg.message
     );
 }
+
 function createSystemFrom(msg) {
     return $('<system></system>').append(
         convertTimeToElement(msg),
         msg.message
     );
 }
+
 function onchat(msg) {
     addToChat(createChatFrom(msg));
+    packets.push(['chat', msg]);
 }
+
 function onsystem(msg) {
     addToChat(createSystemFrom(msg));
+    packets.push(['system', msg]);
 }
+
 function onphase(msg) {
     $('h1').text(msg.name);
+    chat.children().remove();
+    packets.push(['phase', msg]);
+}
+
+function cancelform (event) {
+    event.preventDefault();
+    return false;
 }
 
 $(_=>{
@@ -66,16 +79,23 @@ $(_=>{
         return false;
     });
     chat = $('.chat');
+    $('.details form').submit(
+        cancelform
+    ).find('input[type="submit"].playerup').click(_=>{
+        socket.emit('presence', {'player': true});
+    });
     socket = io();
     socket.on('connect', ()=>{
         socket.emit('handshake', {'roomId': roomid, 'usertoken': $('meta[name="session"]').attr('content')});
     });
     socket.on('handshake', events=>{
+        packets = events;
         let currentphaseind = events.map(e=>e[0]=='phase').lastIndexOf(true);
         let currentevents = events.slice(0, currentphaseind);
         if (currentphaseind != -1) {
             let currentphase = events[currentphaseind];
             onphase({'name': currentphase});
+            $('.details form').remove();
         }
         for (let [type, msg] of currentevents) {
             switch (type) {
@@ -109,4 +129,57 @@ $(_=>{
             )
         );
     });
+    socket.on('presence', msg=>{
+        if (msg.player)
+            $('input[type="submit"].playerup').prop('value', 'Player down');
+        else
+            $('input[type="submit"].playerup').prop('value', 'Player up');
+        if (msg.host)
+            $('.details form').append(
+                $('<input>').prop('type', 'submit')
+                            .prop('value', 'Edit Game logic')
+                            .addClass('gamelogic')
+                            .click(_=>{
+                                $('body').addClass('dark')
+                                         .click(_=>{
+                                            if (_.target==document.body)
+                                                $('body').removeClass('dark')
+                                                            .off('click')
+                                                            .find('form.centered')
+                                                            .remove();
+                                        });
+                                $('<form>').submit(cancelform)
+                                            .addClass('centered')
+                                            .append(
+                                                $('<fieldset>').append(
+                                                    $('<legend>').text('Edit Game logic'),
+                                                    $('<label>').prop('for', 'gamelogiclink')
+                                                                .text('Pastebin Link:'),
+                                                    $('<br>'),
+                                                    $('<input>').prop({
+                                                                    type: 'text',
+                                                                    id: 'gamelogiclink'
+                                                                }),
+                                                    $('<br>'),
+                                                    $('<input>').prop({
+                                                                    type: 'submit',
+                                                                    value: 'Change logic'
+                                                                }).click(_=>{
+                                                                    socket.emit('logic', {
+                                                                        'link': $('form.centered input[type="text"]').prop('value')
+                                                                    });
+                                                                    $('body').click();
+                                                                })
+                                                )
+                                            )
+                            }),
+                $('<input>').prop('type', 'submit')
+                            .prop('value', 'Start game')
+                            .addClass('startgame')
+                            .click(_=>{
+                                socket.emit('start');
+                            })
+            );
+    });
+    socket.on('start', ()=>{}); // Future uses?
 });
