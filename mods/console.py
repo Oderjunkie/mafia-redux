bb3e6e327a977619741c7eab17d75869fb2ea9dd
Oderjunkie <raed.tayyib@gmail.com>
2021-07-14 18:41:45 +0300

    fix rich

--- mods/console.py
+++ mods/console.py
@@ -1,5 +1,13 @@
 from rich.console import Console
 console = Console()
-consoleerr = Console(stderr=True)
+consoleerr = console
+try:
+    consoleerr = Console(stderr=True)
+except TypeError:
+    try:
+        from sys import stderr
+        consoleerr = Console(file=stderr)
+    except Exception:
+        pass
 print = console.log
 printerr = consoleerr.log
