1a71a520cab6112e607cc702ae8b79375e996095
Oderjunkie <raed.tayyib@gmail.com>
2021-08-01 21:18:06 +0300

    Ugly fix but works

--- mods/setupflask.py
+++ mods/setupflask.py
@@ -1,5 +1,3 @@
-from mods.mscript import maflogic
-from mods.stdlib import mafstdlib
 from mods.utilities import randString, ReverseProxied
 from flask_socketio import SocketIO
 from flask import Flask
@@ -11,6 +9,9 @@ app.wsgi_app = ReverseProxied(app.wsgi_app)
 app.config['SECRET_KEY'] = randString(100)
 socketio = SocketIO(app)
 client = pymongo.MongoClient(os.environ['MONGODB'])
+
+from mods.stdlib import mafstdlib
+from mods.mscript import maflogic
 stdlibs: dict[str, mafstdlib] = {}
 logics: dict[str, maflogic] = {}
 sessions: dict[str, str] = {}
