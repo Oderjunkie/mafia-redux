292605faea2dfb61f2272b2adf3b4c651c3d30dc
Oderjunkie <raed.tayyib@gmail.com>
2021-08-01 19:24:09 +0300

    Add jsdocstrings

--- static/javascript/specific.js
+++ static/javascript/specific.js
@@ -26,6 +26,13 @@ let roomid, socket, button, textbox, form, chat, packets, ishost;
  * 7/21/2021 00:13
  */
 
+/**
+ * Converts a user id into a username.
+ * 
+ * @param {string} id - The userid.
+ * @returns {Promise<string>}
+ */
+
 function getNameFromId(id) {
     return new Promise(resolve =>
         $.post('/graphql', {query: '{ user(id:' + JSON.stringify(id) + ') { username } }'}, res=>{
@@ -39,6 +46,15 @@ const encodeGUIname = btoa;
 // function encodeGUIname(name) {
 //     return name.toLowerCase().replace(/[^a-z]/g, '');
 // }
+
+/**
+ * Creates a GUI box based on a SocketIO event.
+ * 
+ * @param {Object} msg - The event.
+ * @param {string} msg.name - The name of the GUI box.
+ * @param {Object<string, *>} msg.list - An object containing the names and values of all options.
+ * @param {string} msg.optional - Whether or not the GUI box has a "No selection" option.
+ */
 
 function createGUI({name, list: selection, optional}) {
     $('.details form:last').before(
@@ -58,13 +74,29 @@ function createGUI({name, list: selection, optional}) {
     );
 }
 
+/**
+ * Freezes a specific GUI box from a SocketIO event.
+ * 
+ * @param {string} name - Name of the GUI box.
+ */
+
 function freezeGUI(name) {
     $('form[formname="' + encodeGUIname(name) + '"] > fieldset > select').prop('disabled', true);
 }
 
+/**
+ * Deletes a specific GUI box from a SocketIO event.
+ * 
+ * @param {string} name - Name of the GUI box.
+ */
+
 function deleteGUI(name) {
     $('form[formname="' + encodeGUIname(name) +'"]').remove();
 }
+
+/**
+ * Checks if the chat textbox has text in it or not.
+ */
 
 function checkforsubmission() {
     let text = textbox.val();
@@ -74,14 +106,29 @@ function checkforsubmission() {
         button.prop('disabled', true);
 }
 
-function convertTimeToElement(msg) {
-    let date = new Date(msg.timestamp*1000);
-    $('<time></time>').append(
+/**
+ * Gets the time of a SocketIO event.
+ * 
+ * @param {Object} msg - The SocketIO event.
+ * @param {string} msg.timestamp - The Unix Timestamp of the event.
+ * @returns {jQuery}
+ */
+
+function convertTimeToElement({timestamp}) {
+    let date = new Date(timestamp*1000);
+    return $('<time></time>').append(
         date.getHours().toString().padStart(2, '0')+':'+
         date.getMinutes().toString().padStart(2, '0')+':'+
         date.getSeconds().toString().padStart(2, '0')
     );
 }
+
+/**
+ * Appends an element to chat, and appropriately scrolls
+ * the chat to the bottom.
+ * 
+ * @param {jQuery} el - The element to appent
+ */
 
 function addToChat(el) {
     let chatobj = chat[0];
@@ -93,6 +140,16 @@ function addToChat(el) {
     }
 }
 
+/**
+ * Logs a SocketIO "chat" event.
+ * 
+ * @async
+ * @param {Object} msg - The SocketIO event.
+ * @param {string} msg.fromid - The userid of the author.
+ * @param {string} msg.message - The contents of the message.
+ * @returns {jQuery}
+ */
+
 async function createChatFrom(msg) {
     let from = await getNameFromId(msg.fromid);
     return $('<message></message>').append(
@@ -102,6 +159,14 @@ async function createChatFrom(msg) {
     );
 }
 
+/**
+ * Logs a SocketIO "system" event.
+ * 
+ * @param {Object} msg - The SocketIO event.
+ * @param {string} msg.message - The contents of the message.
+ * @returns {jQuery}
+ */
+
 function createSystemFrom(msg) {
     return $('<system></system>').append(
         convertTimeToElement(msg),
@@ -109,15 +174,42 @@ function createSystemFrom(msg) {
     );
 }
 
+/**
+ * Wrapper around `createChatFrom`, to add it
+ * to chat and append it to the packet log.
+ * 
+ * @async
+ * @param {Object} msg
+ * @param {string} msg.fromid - The userid of the author.
+ * @param {string} msg.message - The contents of the message.
+ */
+
 async function onchat(msg) {
     addToChat(await createChatFrom(msg));
     packets.push(['chat', msg]);
 }
 
+/**
+ * Wrapper around `createSystemFrom`, to add it
+ * to chat and append it to the packet log.
+ * 
+ * @async
+ * @param {Object} msg
+ * @param {string} msg.fromid - The userid of the author.
+ * @param {string} msg.message - The contents of the message.
+ */
+
 async function onsystem(msg) {
     addToChat(await createSystemFrom(msg));
     packets.push(['system', msg]);
 }
+
+/**
+ * Creates a pop-up box.
+ * 
+ * @param {string} name - The legend of the form.
+ * @param {jQuery[]} appendables - The contents of the form.
+ */
 
 function createPopup(name, appendables) {
     $('body').addClass('dark')
@@ -139,21 +231,47 @@ function createPopup(name, appendables) {
     )
 }
 
+/**
+ * Changes the current phase.
+ * 
+ * @param {string} name - The name of the new phase.
+ */
+
 function onphase(name) {
     $('h1').text(name);
     chat.children().remove();
     packets.push(['phase', name]);
 }
 
+/**
+ * Cancels form submissions.
+ * 
+ * @param {*} event - The event caused by the submission.
+ * @returns {boolean} - `false`, to disable the form.
+ */
+
 function cancelform(event) {
     event.preventDefault();
     return false;
 }
 
+/**
+ * Automatically sends messages when pressing enter.
+ * 
+ * @param {*} event - The event caused by the `onmousedown` hook.
+ */
+
 function autosend(event) {
     if (event.which==13)
         button.click();
 }
+
+/**
+ * Sends a message.
+ * 
+ * @param {*} event - The event caused by the `onsubmit` hook.
+ * @returns {boolean} - `false`, to disable the form.
+ */
 
 function chatout(event) {
     socket.emit('chat', textbox.val());
@@ -163,11 +281,26 @@ function chatout(event) {
     return false;
 }
 
+/**
+ * Displays an error message in the form of a
+ * pop-up box [using `createPopup`]
+ * 
+ * @param {string} msg - The error message to display.
+ */
+
 function handleError(msg) {
     createPopup('Error', [
         $('<p>').text(msg)
     ]);
 }
+
+/**
+ * Updates the client's GUI to reflect their presence within the room.
+ * 
+ * @param {Object} msg - The SocketIO event.
+ * @param {boolean} msg.player - Whether or not the client is playered up.
+ * @param {boolean} msg.host - Whether or not the client is hosting the room.
+ */
 
 function updatepresence(msg) {
     if (msg.player)
@@ -211,6 +344,12 @@ function updatepresence(msg) {
     }
 }
 
+/**
+ * Handles the client's side of the handshake process.
+ * 
+ * @param {*} msg - The events.
+ */
+
 async function onhandshake(events) {
     let currentphaseind = events.map(e=>e[0]=='phase').lastIndexOf(true);
     let currentevents = events.slice(0, currentphaseind);
@@ -229,6 +368,11 @@ async function onhandshake(events) {
                 break;
         }
 }
+
+/**
+ * The main function.
+ * Not very documentable, as it shouldn't be called.
+ */
 
 function main() {
     roomid = location.pathname.split('/')[2];
