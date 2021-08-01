2f2b51bed48362ae21cc5d98089a672036c8d7a4
Oderjunkie <raed.tayyib@gmail.com>
2021-08-01 21:20:31 +0300

    OH GOD THIS IS SO UNPYTHONIC AAAAAA

--- mods/setupflask.py
+++ mods/setupflask.py
@@ -1,5 +1,6 @@
 from mods.utilities import randString, ReverseProxied
 from flask_socketio import SocketIO
+from mods.mscript import maflogic
 from flask import Flask
 import pymongo
 import os
@@ -10,9 +11,8 @@ app.config['SECRET_KEY'] = randString(100)
 socketio = SocketIO(app)
 client = pymongo.MongoClient(os.environ['MONGODB'])
 
-from mods.stdlib import mafstdlib
-from mods.mscript import maflogic
-stdlibs: dict[str, mafstdlib] = {}
-logics: dict[str, maflogic] = {}
 sessions: dict[str, str] = {}
 usersinrooms: dict = {}
+from mods.stdlib import mafstdlib
+stdlibs: dict[str, mafstdlib] = {}
+logics: dict[str, maflogic] = {}
