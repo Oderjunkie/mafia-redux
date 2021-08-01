80ff0ebe09a2018859ac40393b1af054d7390750
Oderjunkie <raed.tayyib@gmail.com>
2021-08-01 16:07:58 +0300

    Add type annotations

--- mods/setupflask.py
+++ mods/setupflask.py
@@ -1,3 +1,5 @@
+from mods.mscript import maflogic
+from mods.stdlib import mafstdlib
 from mods.utilities import randString, ReverseProxied
 from flask_socketio import SocketIO
 from flask import Flask
@@ -9,7 +11,7 @@ app.wsgi_app = ReverseProxied(app.wsgi_app)
 app.config['SECRET_KEY'] = randString(100)
 socketio = SocketIO(app)
 client = pymongo.MongoClient(os.environ['MONGODB'])
-stdlibs = {}
-logics = {}
-sessions = {}
-usersinrooms = {}
+stdlibs: dict[str, mafstdlib] = {}
+logics: dict[str, maflogic] = {}
+sessions: dict[str, str] = {}
+usersinrooms: dict = {}
